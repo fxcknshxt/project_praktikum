86e729c64c0e624abb93cbbbdc7c049fd493015b
Alexey Nafikov <alexeysg2@gmail.com>
2026-03-27 01:51:13 +0500

    создание преокта Spring Boot с зависимостями

---
+++ campusjobs/src/main/java/com/university/campusjobs/CampusjobsApplication.java
@@ -0,0 +1,13 @@
+package com.university.campusjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusjobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusjobsApplication.class, args);
+	}
+
+}
---
+++ campusjobs/src/test/java/com/university/campusjobs/CampusjobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.campusjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusjobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
